0d4380140ab61f0ed819bb12df1020bcd8618b43
Goldyn19 <nwaezegolden2002@gmail.com>
2025-03-10 15:16:40 +0100

    adjusted redirect logic

--- app/[user]/[shortenURL]/page.tsx
+++ app/[user]/[shortenURL]/page.tsx
@@ -1,17 +1,25 @@
 import { redirect } from "next/navigation";
 import LinkNotFound from "@/app/components/LinkNotFound";
 
-// Simulate fetching original URL from a database
-async function getOriginalUrl(shortUrl: string, user_id: string): Promise<string | null> {
-    console.log(user_id, shortUrl ) ;
-  const res = await fetch(`http://localhost:8000/link/urls/${user_id}/${shortUrl}`); // Replace with your actual API
-  if (!res.ok) return null;
-  const data = await res.json();
-  return data.original_url; // Ensure your API returns { originalUrl: "https://example.com" }
+interface PageProps {
+  params: {
+    user: string;
+    shortenURL: string;
+  };
 }
 
-export default async function ShortUrlPage({ params }: { params: { user: string; shortenURL: string  } }) {
-    
+const url = `${process.env.NEXT_PUBLIC_API_URL}`;
+
+// Simulate fetching original URL from a database
+async function getOriginalUrl(shortUrl: string, user_id: string): Promise<string | null> {
+  console.log(user_id, shortUrl);
+  const res = await fetch(`${url}/link/urls/${user_id}/${shortUrl}`); // Replace with your actual API
+  if (!res.ok) return null;
+  const data = await res.json();
+  return data.original_url; // Ensure your API returns { original_url: "https://example.com" }
+}
+
+export default async function ShortUrlPage({ params }: PageProps) {
   const originalUrl = await getOriginalUrl(params.shortenURL, params.user);
 
   if (!originalUrl) {
